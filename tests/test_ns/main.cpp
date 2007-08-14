d83260623c6bb353468b5dd8a6ddb9d0ff8e47c1
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 21:20:02 +0000

    date	2005.04.08.10.34.00;	author kalmar;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2512 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ tests/test_ns/main.cpp
@@ -0,0 +1,50 @@
+// test_ns.cpp : Defines the entry point for the console application.
+//
+
+
+#include "test_ns.h"
+
+void writeTest(const std::string& fname)
+{
+
+	Udm::SmartDataNetwork  out(test_ns::diagram);
+  out.CreateNew(fname.c_str(), "AB.xsd",	test_ns::AB::AB::meta, Udm::CHANGES_PERSIST_ALWAYS);
+
+  test_ns::AB::AB ab = test_ns::AB::AB::Cast(out.GetRootObject());
+  test_ns::test_ns::B1 b1 = test_ns::test_ns::B1::Create(ab);
+  test_ns::AB::AB1 ab1 = test_ns::AB::AB1::Create(ab);
+
+	out.CloseWithUpdate();
+}
+
+//==========================================
+void readTest(const std::string& fname)
+{
+
+	Udm::SmartDataNetwork  out(test_ns::diagram);
+  out.OpenExisting(fname.c_str(), "test_ns.xsd");
+
+  
+  test_ns::AB::AB ab = test_ns::AB::AB::Cast(out.GetRootObject());
+
+	
+  out.CloseNoUpdate();
+}
+//=========================================
+
+void main()
+{
+  try
+  {
+    std::string fn ="test.xml";
+    writeTest(fn);
+    readTest(fn);
+  }
+  catch(udm_exception& e)
+  {
+    std::cout << e.what();
+  }
+
+	
+}
+
