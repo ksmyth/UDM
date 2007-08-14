1400531033d7e18aef08dd24b88efec0a6b6cb59
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:35:00 +0000

    date	2005.06.23.06.19.00;	author nsomogyi;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1753 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ etc/JAVA/eclipseAPI.bat
@@ -0,0 +1,24 @@
+cls
+
+@echo off
+
+rem if "%3"=="" goto usage
+
+chdir > d
+set /p curr_dir=<"d"
+
+echo Generate domain-specific %1 jUdm source ...
+%UDM_PATH%\bin.net\udm.exe %2 -java %3
+echo Done
+
+echo Generate the domain-specific %1 eclipse project ...
+%UDM_PATH%\judm\lib\ant-1.6.2\bin\ant -f %UDM_PATH%\etc\java\etc\build.xml -Dbasedir=%curr_dir% -Ddspackage=%1 -quiet eclipse
+echo Done
+
+:usage
+echo .
+echo eclipseApi: Wrong parameter!
+echo Usage:   %%UDM_PATH%%\etc\java\judmAPI [eclipse_project_name] [UML_meta_model] [UDM_parameters]
+echo Example: %%UDM_PATH%%\etc\java\judmAPI SBML2Ex SBML2Ex_udm.xml "-i jigcell -q jigcell -e"
+goto end
+:end
