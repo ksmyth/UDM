18b5cf2edb00b8ad9f07a57f1caf83767c530b97
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:40:01 +0000

    -D option is not necessary anymore date	2005.04.25.16.51.00;	author nsomogyi;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1877 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/etc/runtests.cmd
+++ judm/etc/runtests.cmd
@@ -1,4 +1,2 @@
 @rem Used in domain-specific packages to run unit tests
-@rem Necessary to pick up DLLs from judm dir
-@set PATH=%PATH%;..\judm
-@java -Djava.library.path=..\judm -jar @DSPACKAGE@-test.jar
+@java -jar @DSPACKAGE@-test.jar
--- judm/etc/runtests.sh
+++ judm/etc/runtests.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
 #Used in domain-specific packages to run unit tests
-export LD_LIBRARY_PATH=../judm
 
-java -Djava.library.path=../judm -jar @DSPACKAGE@-test.jar
+java -jar @DSPACKAGE@-test.jar
