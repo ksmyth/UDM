a8e8463dd3ebefbb1021a6ddb13763a5f9778bb3
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:50:57 +0000

    date	2005.06.07.16.05.00;	author endre;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2049 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ src/Udm/UdmUtil.cpp
@@ -0,0 +1,62 @@
+/*
+Copyright (c) Vanderbilt University, 2000-2005
+ALL RIGHTS RESERVED
+Vanderbilt University disclaims all warranties with regard to this
+software, including all implied warranties of merchantability
+and fitness.  In no event shall Vanderbilt University be liable for
+any special, indirect or consequential damages or any damages
+whatsoever resulting from loss of use, data or profits, whether
+in an action of contract, negligence or other tortious action,
+arising out of or in connection with the use or performance of
+this software.
+*/
+
+#include "Udm.h"
+#include "UdmBase/DTDGen.h"
+
+#include <time.h>
+#include <fstream>
+
+std::string GetTime()	{
+		time_t t;
+		time(&t);
+		char *c = ctime(&t);
+
+		c[24] = '\0';
+		return c;
+}
+
+std::string NameToFilename(const std::string src)
+{
+	std::string ret = src;
+	for (std::string::iterator i = ret.begin(); i != ret.end(); i++) {
+		if (!isalnum(*i)) *i = '_';
+	}
+	return ret;
+}
+
+void GenerateDSD(const ::Uml::Uml::Namespace &ns,
+		 const string &fname,
+		 bool generate_dtd,
+		 bool uxsdi,
+		 bool xsd_el_ta,
+		 const map<string, string> &ns_map,
+		 const set<string> &ns_ignore_set,
+		 bool qualifiedAtrrsNS)
+{
+	ofstream ff;
+
+	if (generate_dtd)
+	{
+		ff.open( (fname+".dtd").c_str() );
+		if(!ff.good()) throw udm_exception("Error opening for write " + fname + ".dtd");
+		else DTDGen::GenerateDTD(ns, ff);
+		ff.close();
+		ff.clear();
+	}
+
+	ff.open( (fname+".xsd").c_str() );
+	if(!ff.good()) throw udm_exception("Error opening for write " + fname + ".xsd");
+	else DTDGen::GenerateXMLSchema(ns, ff, ns_map, ns_ignore_set, uxsdi, xsd_el_ta, qualifiedAtrrsNS);
+	ff.close();
+}
