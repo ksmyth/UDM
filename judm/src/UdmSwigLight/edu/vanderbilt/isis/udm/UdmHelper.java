7b5481ae79b0b9f26e61f468cd02eb7381629fcf
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:46:11 +0000

    $/MoBIESTransition/udm_ns/judm/src/UdmSwigLight/edu/vanderbilt/isis/udm Comment: UdmCint -> UdmSwig date	2005.05.24.15.11.00;	author nsomogyi;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1905 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/src/UdmSwigLight/edu/vanderbilt/isis/udm/UdmHelper.java
+++ judm/src/UdmSwigLight/edu/vanderbilt/isis/udm/UdmHelper.java
@@ -5,7 +5,7 @@ package edu.vanderbilt.isis.udm;
 
 import java.util.logging.Logger;
 
-import edu.vanderbilt.isis.udm.swig.UDMCint;
+import edu.vanderbilt.isis.udm.swig.UDMSwig;
 import edu.vanderbilt.isis.udm.swig.cint_string;
 
 /**
@@ -15,18 +15,18 @@ import edu.vanderbilt.isis.udm.swig.cint_string;
  */
 public class UdmHelper {
     private static final Logger log = Logger.getLogger(UdmHelper.class.getName());
-    public static int TARGET_FROM_PEER = UDMCint.getTARGETFROMPEER();
-    public static int TARGET_FROM_CLASS = UDMCint.getTARGETFROMCLASS();
-    public static int CLASS_FROM_TARGET = UDMCint.getCLASSFROMTARGET();
-    public static int UPDN_CHANGES_PERSIST_ALWAYS = UDMCint.getUPDN_CHANGES_PERSIST_ALWAYS();
-    public static int UPDN_CHANGES_PERSIST_DEFAULT = UDMCint.getUPDN_CHANGES_PERSIST_DEFAULT();
-    public static int UPDN_CHANGES_LOST_DEFAULT = UDMCint.getUPDN_CHANGES_LOST_DEFAULT();
+    public static int TARGET_FROM_PEER = UDMSwig.getTARGETFROMPEER();
+    public static int TARGET_FROM_CLASS = UDMSwig.getTARGETFROMCLASS();
+    public static int CLASS_FROM_TARGET = UDMSwig.getCLASSFROMTARGET();
+    public static int UPDN_CHANGES_PERSIST_ALWAYS = UDMSwig.getUPDN_CHANGES_PERSIST_ALWAYS();
+    public static int UPDN_CHANGES_PERSIST_DEFAULT = UDMSwig.getUPDN_CHANGES_PERSIST_DEFAULT();
+    public static int UPDN_CHANGES_LOST_DEFAULT = UDMSwig.getUPDN_CHANGES_LOST_DEFAULT();
 
     public static UdmPseudoObject loadDiagram(String xmlMetaFile) throws UdmException {
         log.finer("UdmPseudoObject swigUpo = new UdmPseudoObject();");
         edu.vanderbilt.isis.udm.swig.UdmPseudoObject swigUpo = new edu.vanderbilt.isis.udm.swig.UdmPseudoObject();
-        log.finer("UDMCint.UPO_LoadDiagram(\"" + xmlMetaFile + "\",swigUpo);");
-        boolean success = UDMCint.UPO_LoadDiagram(xmlMetaFile, swigUpo);
+        log.finer("UDMSwig.UPO_LoadDiagram(\"" + xmlMetaFile + "\",swigUpo);");
+        boolean success = UDMSwig.UPO_LoadDiagram(xmlMetaFile, swigUpo);
         checkError(success, "Error while loading metaDiagram file: " + xmlMetaFile);
         return new UdmPseudoObject(swigUpo);
     }
@@ -34,36 +34,36 @@ public class UdmHelper {
     public static UdmPseudoObject loadDiagramFromString(String xmlMetaFile, String xmlString) throws UdmException {
         log.finer("UdmPseudoObject swigUpo = new UdmPseudoObject();");
         edu.vanderbilt.isis.udm.swig.UdmPseudoObject swigUpo = new edu.vanderbilt.isis.udm.swig.UdmPseudoObject();
-        log.finer("UDMCint.UPO_LoadDiagramFromString(\"" + xmlMetaFile + "\"," + xmlString + "\",swigUpo);");
-        boolean success = UDMCint.UPO_LoadDiagramFromString(xmlMetaFile, xmlString, swigUpo);
+        log.finer("UDMSwig.UPO_LoadDiagramFromString(\"" + xmlMetaFile + "\"," + xmlString + "\",swigUpo);");
+        boolean success = UDMSwig.UPO_LoadDiagramFromString(xmlMetaFile, xmlString, swigUpo);
         checkError(success, "Error while loading metaDiagram file from string: " + xmlMetaFile);
         return new UdmPseudoObject(swigUpo);
     }
 
     public static void unloadDiagram(String xmlMetaFile) throws UdmException {
-        log.finer("UDMCint.UPO_UnLoadDiagram(\"" + xmlMetaFile + "\");");
-        boolean success = UDMCint.UPO_UnLoadDiagram(xmlMetaFile);
+        log.finer("UDMSwig.UPO_UnLoadDiagram(\"" + xmlMetaFile + "\");");
+        boolean success = UDMSwig.UPO_UnLoadDiagram(xmlMetaFile);
         checkError(success, "Error while unloading metaDiagram file: " + xmlMetaFile);
     }
 
     public static void UPO_SetDiagram(UdmPseudoObject diagramWhat, String name) throws UdmException {
-        log.finer("UDMCint.UPO_SetDiagram(diagram,\"" + name + "\");");
-        boolean success = UDMCint.UPO_SetDiagram(diagramWhat.getInternal(), name);
+        log.finer("UDMSwig.UPO_SetDiagram(diagram,\"" + name + "\");");
+        boolean success = UDMSwig.UPO_SetDiagram(diagramWhat.getInternal(), name);
         checkError(success, "Error in UDM method UPO_SetDiagram()");
     }
 
     public static void UPO_SetClass(UdmPseudoObject classWhat, UdmPseudoObject diagramWhatDgr,
             String targetName) throws UdmException {
-        log.finer("UDMCint.UPO_SetClass(class,diagram,\"" + targetName + "\");");
-        boolean success = UDMCint.UPO_SetClass(classWhat.getInternal(), diagramWhatDgr
+        log.finer("UDMSwig.UPO_SetClass(class,diagram,\"" + targetName + "\");");
+        boolean success = UDMSwig.UPO_SetClass(classWhat.getInternal(), diagramWhatDgr
                 .getInternal(), targetName);
         checkError(success, "Error in UDM method UPO_SetClass");
     }
 
     public static void UPO_SetAttribute(UdmPseudoObject attrWhat, UdmPseudoObject classWhatClass,
             String targetName) throws UdmException {
-        log.finer("UDMCint.UPO_SetAttribute(attr,class,\"" + targetName + "\");");
-        boolean success = UDMCint.UPO_SetAttribute(attrWhat.getInternal(), classWhatClass
+        log.finer("UDMSwig.UPO_SetAttribute(attr,class,\"" + targetName + "\");");
+        boolean success = UDMSwig.UPO_SetAttribute(attrWhat.getInternal(), classWhatClass
                 .getInternal(), targetName);
         checkError(success, "Error in UDM method UPO_SetAttribute");
     }
@@ -71,9 +71,9 @@ public class UdmHelper {
     public static void UPO_SetChildRole(UdmPseudoObject ccrWhat, UdmPseudoObject whatTargetClass,
             UdmPseudoObject whatTheoTargetClass, String roleName, String oRoleName)
             throws UdmException {
-        log.finer("UDMCint.UPO_SetChildRole(ccr,targetClass,theoTargetClass,\"" + roleName
+        log.finer("UDMSwig.UPO_SetChildRole(ccr,targetClass,theoTargetClass,\"" + roleName
                 + "\",\"" + oRoleName + "\");");
-        boolean success = UDMCint.UPO_SetChildRole(ccrWhat.getInternal(), whatTargetClass
+        boolean success = UDMSwig.UPO_SetChildRole(ccrWhat.getInternal(), whatTargetClass
                 .getInternal(), whatTheoTargetClass.getInternal(), roleName, oRoleName);
         checkError(success, "Error in UDM method UPO_SetChildRole");
     }
@@ -81,18 +81,18 @@ public class UdmHelper {
     public static void UPO_SetParentRole(UdmPseudoObject cprWhat, UdmPseudoObject whatTargetClass,
             UdmPseudoObject whatTheoTargetClass, String roleName, String oRoleName)
             throws UdmException {
-        log.finer("UDMCint.UPO_SetParentRole(cpr,targetClass,theoTargetClass,\"" + roleName
+        log.finer("UDMSwig.UPO_SetParentRole(cpr,targetClass,theoTargetClass,\"" + roleName
                 + "\",\"" + oRoleName + "\");");
-        boolean success = UDMCint.UPO_SetParentRole(cprWhat.getInternal(), whatTargetClass
+        boolean success = UDMSwig.UPO_SetParentRole(cprWhat.getInternal(), whatTargetClass
                 .getInternal(), whatTheoTargetClass.getInternal(), roleName, oRoleName);
         checkError(success, "Error in UDM method UPO_SetParentRole");
     }
 
     public static void UPO_SetAssocRole(UdmPseudoObject arWhat, UdmPseudoObject whatTargetClass,
             UdmPseudoObject whatTheoTargetClass, String targetName) throws UdmException {
-        log.finer("UDMCint.UPO_SetAssocRole(ar,targetClass,theoTargetClass,\"" + targetName
+        log.finer("UDMSwig.UPO_SetAssocRole(ar,targetClass,theoTargetClass,\"" + targetName
                 + "\");");
-        boolean success = UDMCint.UPO_SetAssocRole(arWhat.getInternal(), whatTargetClass
+        boolean success = UDMSwig.UPO_SetAssocRole(arWhat.getInternal(), whatTargetClass
                 .getInternal(), whatTheoTargetClass.getInternal(), targetName);
         checkError(success, "Error in UDM method UPO_SetAssocRole");
     }
@@ -100,7 +100,7 @@ public class UdmHelper {
     {
         cint_string key_c = new cint_string(key);
         cint_string xsd_str_c = new cint_string(xsd_str);
-        boolean success = UDMCint.StoreXsd(key_c, xsd_str_c);
+        boolean success = UDMSwig.StoreXsd(key_c, xsd_str_c);
         checkError(success, "Error in UDM method StoreXsd");
     }
     
@@ -110,7 +110,7 @@ public class UdmHelper {
      */
     public static void ClearXsdStorage()throws UdmException 
     {
-        boolean success = UDMCint.ClearXsdStorage();
+        boolean success = UDMSwig.ClearXsdStorage();
         checkError(success, "Error in UDM method ClearXsdStorage");
     }
     
@@ -118,7 +118,7 @@ public class UdmHelper {
     public static void RemoveXsd(String key)throws UdmException 
     {
     	cint_string key_c = new cint_string(key);
-        boolean success = UDMCint.RemoveXsd(key_c);
+        boolean success = UDMSwig.RemoveXsd(key_c);
         checkError(success, "Error in UDM method RemoveXsd");
     	
     }
@@ -133,7 +133,7 @@ public class UdmHelper {
     {
         cint_string ns_uri = new cint_string(namespaceURI);
         cint_string ns_uml = new cint_string(namespaceUML);
-        boolean success = UDMCint.AddURIToUMLNamespaceMapping(ns_uri, ns_uml);
+        boolean success = UDMSwig.AddURIToUMLNamespaceMapping(ns_uri, ns_uml);
         checkError(success, "Error in UDM method AddURIToUMLNamespaceMapping");
       }
 
@@ -148,13 +148,13 @@ public class UdmHelper {
         
         log.finer("UdmPseudoObject swigNewUPO = new UdmPseudoObject();");
         edu.vanderbilt.isis.udm.swig.UdmPseudoObject swigNewUPO = new edu.vanderbilt.isis.udm.swig.UdmPseudoObject();
-        log.finer("UDMCint.UPO_SetClass(swigNewUPO,diagram,\"" + className + "\");");
+        log.finer("UDMSwig.UPO_SetClass(swigNewUPO,diagram,\"" + className + "\");");
         edu.vanderbilt.isis.udm.swig.UdmPseudoObject nsUPO = new edu.vanderbilt.isis.udm.swig.UdmPseudoObject();
         
-        boolean success = UDMCint.UPO_SetNamespace(nsUPO, diagram.getInternal(), nsName);
+        boolean success = UDMSwig.UPO_SetNamespace(nsUPO, diagram.getInternal(), nsName);
         UdmHelper.checkError(success, "UDM method UPO_SetNamespace() failed");
         
-        success = UDMCint.UPO_SetClass(swigNewUPO, nsUPO, className);
+        success = UDMSwig.UPO_SetClass(swigNewUPO, nsUPO, className);
         UdmHelper.checkError(success, "UDM method UPO_SetClass() failed");
         return new UdmPseudoObject(swigNewUPO);
     }
@@ -184,7 +184,7 @@ public class UdmHelper {
 
     public static void initialize() throws Exception {
         try {
-            System.loadLibrary("UdmCint");
+            System.loadLibrary("UDMSwig");
         } catch (Exception e) {
             throw new RuntimeException("Error while initializing UDM DLLs through JNI", e);
         }
