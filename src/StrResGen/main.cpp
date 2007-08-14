3a9f56aacdca8e42bfc390080f336605c3765543
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:49:11 +0000

    date	2005.05.20.15.25.00;	author kalmar;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2041 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ src/StrResGen/main.cpp
@@ -0,0 +1,59 @@
+#include <string>
+#include <iostream>
+#include <fstream>
+#include <stdexcept>
+#include <algorithm>
+#include "../Udm/File2Code/File2Code.h"
+//==================================
+int main(int argc, char **argv)
+{
+ 	if(argc < 3)
+	{
+		std::cerr << "Usage: " << argv[0] << " <text_filename> <namespace_name> <JAVA|CPP>" << std::endl;
+		return(-1);
+	}
+
+	try
+	{
+
+    std::string infname(argv[1]);
+    std::string outfname = std::string(argv[2]);
+    std::string ext(".h");
+    File2Code::GenModeType mode(File2Code::CPP);
+    if (argc == 4)
+    {
+      std::string ar(argv[3]);
+      if (ar == "JAVA")
+      {
+        ext = ".java";
+        mode = File2Code::JAVA;
+      }
+      else
+      if (ar == "CPP")
+      {
+        ext = ".h";
+        mode = File2Code::CPP;
+      }
+    }
+    outfname += ext;
+
+    std::ofstream out(outfname.c_str());
+    if (!out.good())
+    {
+      std::string err = outfname + std::string(" could not open.");
+      throw std::runtime_error(err);
+    }
+
+    File2Code f2c(argv[2], infname, mode);
+    f2c.gen(out);
+
+	}
+ 	catch(const std::exception &e)
+	{
+    std::cout << "Exception: " << e.what();
+		return 1;
+
+   }
+
+  return 0;
+}
