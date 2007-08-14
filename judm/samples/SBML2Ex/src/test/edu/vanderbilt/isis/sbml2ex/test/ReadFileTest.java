b8aaa1c1e2db54cacf161cef95e790cb755b28d4
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:46:10 +0000

    $/MoBIESTransition/udm_ns/judm/samples/SBML2Ex/src/test/edu/vanderbilt/isis/sbm l2ex/test Comment: write test added date	2005.04.25.08.24.00;	author nsomogyi;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1899 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/samples/SBML2Ex/src/test/edu/vanderbilt/isis/sbml2ex/test/ReadFileTest.java
+++ judm/samples/SBML2Ex/src/test/edu/vanderbilt/isis/sbml2ex/test/ReadFileTest.java
@@ -5,6 +5,9 @@
  */
 package edu.vanderbilt.isis.sbml2ex.test;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 import junit.framework.TestCase;
 import edu.vanderbilt.isis.sbml2ex.FactoryRepository;
 import edu.vanderbilt.isis.sbml2ex.math.apply;
@@ -12,9 +15,13 @@ import edu.vanderbilt.isis.sbml2ex.math.ci;
 import edu.vanderbilt.isis.sbml2ex.math.cn;
 import edu.vanderbilt.isis.sbml2ex.math.lambda;
 import edu.vanderbilt.isis.sbml2ex.math.math;
+import edu.vanderbilt.isis.sbml2ex.sbml.annotation;
+import edu.vanderbilt.isis.sbml2ex.sbml.compartment;
 import edu.vanderbilt.isis.sbml2ex.sbml.delay;
+import edu.vanderbilt.isis.sbml2ex.sbml.dialect;
 import edu.vanderbilt.isis.sbml2ex.sbml.event;
 import edu.vanderbilt.isis.sbml2ex.sbml.functionDefinition;
+import edu.vanderbilt.isis.sbml2ex.sbml.listOfCompartments;
 import edu.vanderbilt.isis.sbml2ex.sbml.listOfEvents;
 import edu.vanderbilt.isis.sbml2ex.sbml.listOfFunctionDefinitions;
 import edu.vanderbilt.isis.sbml2ex.sbml.listOfSpecies;
@@ -23,6 +30,8 @@ import edu.vanderbilt.isis.sbml2ex.sbml.notes;
 import edu.vanderbilt.isis.sbml2ex.sbml.sbml;
 import edu.vanderbilt.isis.sbml2ex.sbml.sbmlFileFactory;
 import edu.vanderbilt.isis.sbml2ex.sbml.species;
+import edu.vanderbilt.isis.sbml2ex.unigene.acc;
+import edu.vanderbilt.isis.sbml2ex.unigene.cluster_id;
 import edu.vanderbilt.isis.sbml2ex.xhtml.head;
 import edu.vanderbilt.isis.udm.UdmException;
 
@@ -34,10 +43,104 @@ import edu.vanderbilt.isis.udm.UdmException;
  *
  */
 public class ReadFileTest extends TestCase {
+    private static final String NEW_INSTANCE_FILE = "samples/NewEmptyInstanceFile.xml";
+
     /**
-     * Opens an existing data network from a file.
+     * Fills the root object with some dummy data.
+     * @param root
      * @throws UdmException
      */
+    protected void fillSbml(sbml root) throws UdmException {
+        // set the dialect
+        annotation an = annotation.create(root);
+        dialect di = dialect.create(an);
+        di.setname("unigene");
+
+        // create a model
+        model mo = model.create(root);
+
+        // create compartment
+        listOfCompartments li = listOfCompartments.create(mo);
+        compartment co = compartment.create(li);
+        co.setid("compartment");
+
+        // create some species with unigene annotation
+        listOfSpecies lis = listOfSpecies.create(mo);
+        species sp = species.create(lis);
+        sp.setid("AA017353");
+        sp.setcompartment("compartment");
+
+        annotation ann = annotation.create(sp);
+        acc _acc = acc.create(ann);
+        _acc.setvalue("AA017353");
+
+        edu.vanderbilt.isis.sbml2ex.unigene.title t =
+            edu.vanderbilt.isis.sbml2ex.unigene.title.create(ann);
+        t.setvalue("7-dehydrocholesterol reductase");
+
+        cluster_id cid = cluster_id.create(ann);
+        cid.setvalue("Hs.11806");
+
+        edu.vanderbilt.isis.sbml2ex.unigene.gene g =
+            edu.vanderbilt.isis.sbml2ex.unigene.gene.create(ann);
+        g.setvalue("DHCR7");
+
+        // create funtions with some math 
+        listOfFunctionDefinitions lifd = listOfFunctionDefinitions.create(mo);
+        functionDefinition fd = functionDefinition.create(lifd);
+        math ma = math.create(fd);
+        apply appl = apply.create(ma);
+        cn _cn = cn.create(appl);
+        _cn.settype("integer");
+        _cn.setvalue("1");
+    }
+
+    /**
+     * Prints the content of the specified input stream to the output.
+     * @param is
+     * @throws UdmException
+     */
+    protected void printInputStream(InputStream is) throws UdmException {
+        java.io.DataInputStream din = new java.io.DataInputStream(is);
+        String xml = new String("");
+        try {
+            String line = null;
+            while ((line = din.readLine()) != null) {
+                xml += (line + "\n");
+            }
+        } catch (IOException ex) {
+            System.out.println("Error reading from stream: " + ex.getMessage());
+        } finally {
+            //  close stream
+            try {
+                din.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    /**
+     * Creates a new data network in a file.
+     * @throws UdmException
+     */
+    public void testCreateBlankInstance() throws UdmException {
+        sbmlFileFactory gtf = FactoryRepository.getsbmlsbmlFileFactory();
+        sbml root = gtf.create(NEW_INSTANCE_FILE);
+
+        System.out.println("\n------------\n");
+        System.out.println(NEW_INSTANCE_FILE);
+
+        fillSbml(root);
+        printSBML(root);
+
+        gtf.save();
+    }
+
+
+    /**
+      * Opens an existing data network from a file.
+      * @throws UdmException
+      */
     public void testOpenExistingFromFile() throws UdmException {
         System.out.println("\n-----------------------------------");
         System.out.println("\tOpenExistingFromFile");
@@ -76,13 +179,12 @@ public class ReadFileTest extends TestCase {
         readFileTest("samples/l2v1-mc-ode.xml");
         readFileTest("samples/l2v1-units.xml");
 
-        //jigcell 
+        //jigcell
         readFileTest("samples/yeastnew2_core.xml");
         readFileTest("samples/frogegg.xml");
 
         //bc
         readFileTest("samples/Lac_v1.2.2.xml");*/
-
         //dbi, unigene 
         readFileTest("samples/demo_30_paintfb_out.xml");
     }
@@ -138,7 +240,61 @@ public class ReadFileTest extends TestCase {
                 functionDefinition fd = fDefA[i];
                 System.out.println("functionDefinition " + fd.getid());
 
-                math[] mvA = fd.getmathChildren();
+                math m_v = fd.getmathChild();
+
+                apply[] appA = m_v.getapplyChildren();
+                for (int k = 0; k < appA.length; k++) {
+                    apply app = appA[k];
+
+                    cn[] cnA = app.getcnChildren();
+                    for (int l = 0; l < cnA.length; l++) {
+                        cn cn_v = cnA[l];
+                        System.out.println("cntype " + cn_v.gettype());
+                        System.out.println("cnval " + cn_v.getvalue());
+                    }
+
+                    ci[] ciA = app.getciChildren();
+                    for (int l = 0; l < ciA.length; l++) {
+                        ci ci_v = ciA[l];
+                        System.out.println("cival " + ci_v.getvalue());
+                    }
+                }
+
+                lambda[] lmA = m_v.getlambdaChildren();
+                for (int k = 0; k < lmA.length; k++) {
+                    lambda lm_v = lmA[k];
+
+                    apply[] appA2 = lm_v.getapplyChildren();
+                    for (int l = 0; l < appA2.length; l++) {
+                        apply app = appA2[l];
+
+                        cn[] cnA = app.getcnChildren();
+                        for (int x = 0; x < cnA.length; x++) {
+                            cn cn_v = cnA[x];
+                            System.out.println("cntype " + cn_v.gettype());
+                            System.out.println("cnval " + cn_v.getvalue());
+                        }
+
+                        ci[] ciA = app.getciChildren();
+                        for (int x = 0; x < ciA.length; x++) {
+                            ci ci_v = ciA[x];
+                            System.out.println("cival " + ci_v.getvalue());
+                        }
+                    }
+                }
+            }
+        }
+
+        // listOfEvents
+        listOfEvents lEv = m.getlistOfEventsChild();
+        if (lEv != null) {
+            event[] evA = lEv.geteventChildren();
+            for (int i = 0; i < evA.length; i++) {
+                event e_v = evA[i];
+
+                delay d_v = e_v.getdelayChild();
+
+                math[] mvA = d_v.getmathChildren();
                 for (int j = 0; j < mvA.length; j++) {
                     math m_v = mvA[j];
 
@@ -179,66 +335,6 @@ public class ReadFileTest extends TestCase {
                             for (int x = 0; x < ciA.length; x++) {
                                 ci ci_v = ciA[x];
                                 System.out.println("cival " + ci_v.getvalue());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // listOfEvents
-        listOfEvents lEv = m.getlistOfEventsChild();
-        if (lEv != null) {
-            event[] evA = lEv.geteventChildren();
-            for (int i = 0; i < evA.length; i++) {
-                event e_v = evA[i];
-
-                delay[] deA = e_v.getdelayChildren();
-                for (int ii = 0; ii < deA.length; ii++) {
-                    delay d_v = deA[ii];
-
-                    math[] mvA = d_v.getmathChildren();
-                    for (int j = 0; j < mvA.length; j++) {
-                        math m_v = mvA[j];
-
-                        apply[] appA = m_v.getapplyChildren();
-                        for (int k = 0; k < appA.length; k++) {
-                            apply app = appA[k];
-
-                            cn[] cnA = app.getcnChildren();
-                            for (int l = 0; l < cnA.length; l++) {
-                                cn cn_v = cnA[l];
-                                System.out.println("cntype " + cn_v.gettype());
-                                System.out.println("cnval " + cn_v.getvalue());
-                            }
-
-                            ci[] ciA = app.getciChildren();
-                            for (int l = 0; l < ciA.length; l++) {
-                                ci ci_v = ciA[l];
-                                System.out.println("cival " + ci_v.getvalue());
-                            }
-                        }
-
-                        lambda[] lmA = m_v.getlambdaChildren();
-                        for (int k = 0; k < lmA.length; k++) {
-                            lambda lm_v = lmA[k];
-
-                            apply[] appA2 = lm_v.getapplyChildren();
-                            for (int l = 0; l < appA2.length; l++) {
-                                apply app = appA2[l];
-
-                                cn[] cnA = app.getcnChildren();
-                                for (int x = 0; x < cnA.length; x++) {
-                                    cn cn_v = cnA[x];
-                                    System.out.println("cntype " + cn_v.gettype());
-                                    System.out.println("cnval " + cn_v.getvalue());
-                                }
-
-                                ci[] ciA = app.getciChildren();
-                                for (int x = 0; x < ciA.length; x++) {
-                                    ci ci_v = ciA[x];
-                                    System.out.println("cival " + ci_v.getvalue());
-                                }
                             }
                         }
                     }
--- judm/samples/SBML2Ex/src/test/edu/vanderbilt/isis/sbml2ex/test/ReadStringTest.java
+++ judm/samples/SBML2Ex/src/test/edu/vanderbilt/isis/sbml2ex/test/ReadStringTest.java
@@ -6,6 +6,7 @@
 package edu.vanderbilt.isis.sbml2ex.test;
 
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 
 import junit.framework.TestCase;
@@ -15,9 +16,13 @@ import edu.vanderbilt.isis.sbml2ex.math.ci;
 import edu.vanderbilt.isis.sbml2ex.math.cn;
 import edu.vanderbilt.isis.sbml2ex.math.lambda;
 import edu.vanderbilt.isis.sbml2ex.math.math;
+import edu.vanderbilt.isis.sbml2ex.sbml.annotation;
+import edu.vanderbilt.isis.sbml2ex.sbml.compartment;
 import edu.vanderbilt.isis.sbml2ex.sbml.delay;
+import edu.vanderbilt.isis.sbml2ex.sbml.dialect;
 import edu.vanderbilt.isis.sbml2ex.sbml.event;
 import edu.vanderbilt.isis.sbml2ex.sbml.functionDefinition;
+import edu.vanderbilt.isis.sbml2ex.sbml.listOfCompartments;
 import edu.vanderbilt.isis.sbml2ex.sbml.listOfEvents;
 import edu.vanderbilt.isis.sbml2ex.sbml.listOfFunctionDefinitions;
 import edu.vanderbilt.isis.sbml2ex.sbml.listOfSpecies;
@@ -26,6 +31,8 @@ import edu.vanderbilt.isis.sbml2ex.sbml.notes;
 import edu.vanderbilt.isis.sbml2ex.sbml.sbml;
 import edu.vanderbilt.isis.sbml2ex.sbml.sbmlStringFactory;
 import edu.vanderbilt.isis.sbml2ex.sbml.species;
+import edu.vanderbilt.isis.sbml2ex.unigene.acc;
+import edu.vanderbilt.isis.sbml2ex.unigene.cluster_id;
 import edu.vanderbilt.isis.sbml2ex.xhtml.head;
 import edu.vanderbilt.isis.udm.UdmException;
 
@@ -37,7 +44,98 @@ import edu.vanderbilt.isis.udm.UdmException;
  *
  */
 public class ReadStringTest extends TestCase {
- 
+    /**
+     * Fills the root object with some dummy data.
+     * @param root
+     * @throws UdmException
+     */
+    protected void fillSbml(sbml root) throws UdmException {
+        // set the dialect
+        annotation an = annotation.create(root);
+        dialect di = dialect.create(an);
+        di.setname("unigene");
+
+        // create a model
+        model mo = model.create(root);
+
+        // create compartment
+        listOfCompartments li = listOfCompartments.create(mo);
+        compartment co = compartment.create(li);
+        co.setid("compartment");
+
+        // create some species with unigene annotation
+        listOfSpecies lis = listOfSpecies.create(mo);
+        species sp = species.create(lis);
+        sp.setid("AA017353");
+        sp.setcompartment("compartment");
+
+        annotation ann = annotation.create(sp);
+        acc _acc = acc.create(ann);
+        _acc.setvalue("AA017353");
+
+        edu.vanderbilt.isis.sbml2ex.unigene.title t =
+            edu.vanderbilt.isis.sbml2ex.unigene.title.create(ann);
+        t.setvalue("7-dehydrocholesterol reductase");
+
+        cluster_id cid = cluster_id.create(ann);
+        cid.setvalue("Hs.11806");
+
+        edu.vanderbilt.isis.sbml2ex.unigene.gene g =
+            edu.vanderbilt.isis.sbml2ex.unigene.gene.create(ann);
+        g.setvalue("DHCR7");
+
+        // create funtions with some math 
+        listOfFunctionDefinitions lifd = listOfFunctionDefinitions.create(mo);
+        functionDefinition fd = functionDefinition.create(lifd);
+        math ma = math.create(fd);
+        apply appl = apply.create(ma);
+        cn _cn = cn.create(appl);
+        _cn.settype("integer");
+        _cn.setvalue("1");
+    }
+    /**
+     * Prints the content of the specified input stream to the output.
+     * @param is
+     * @throws UdmException
+     */
+    protected void printInputStream(InputStream is) throws UdmException {
+        java.io.DataInputStream din = new java.io.DataInputStream(is);
+        String xml = new String("");
+        try {
+            String line = null;
+            while ((line = din.readLine()) != null) {
+                xml += (line + "\n");
+            }
+        } catch (IOException ex) {
+            System.out.println("Error reading from stream: " + ex.getMessage());
+        } finally {
+            //  close stream
+            try {
+                din.close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    /**
+     * Creates a new data network in a file.
+     * @throws UdmException
+     */
+    public void testCreateBlankInstance() throws UdmException {
+        sbmlStringFactory gtf = FactoryRepository.getsbmlsbmlStringFactory();
+        sbml root = gtf.create();
+
+        System.out.println("\n------------\n");
+        System.out.println("Create blank instance in string");
+        
+        fillSbml(root);
+        printSBML(root);
+
+        String res = gtf.save();
+        System.out.println(res);
+    }
+
+
     /**
      * Opens an existing data network from a file.
      * @throws UdmException
@@ -80,21 +178,20 @@ public class ReadStringTest extends TestCase {
         readStringTest("samples/l2v1-mc-ode.xml");
         readStringTest("samples/l2v1-units.xml");
 
-        //jigcell 
+        //jigcell
         readStringTest("samples/yeastnew2_core.xml");
         readStringTest("samples/frogegg.xml");
 
         //bc
         readStringTest("samples/Lac_v1.2.2.xml");*/
-
         //dbi, unigene 
         readStringTest("samples/demo_30_paintfb_out.xml");
     }
 
-   /**
-     * Opens an existing data network from a file.
-     * @throws UdmException
-     */
+    /**
+      * Opens an existing data network from a file.
+      * @throws UdmException
+      */
     public void readStringTest(String fileName) throws UdmException {
         sbmlStringFactory gtf = FactoryRepository.getsbmlsbmlStringFactory();
 
@@ -154,7 +251,61 @@ public class ReadStringTest extends TestCase {
                 functionDefinition fd = fDefA[i];
                 System.out.println("functionDefinition " + fd.getid());
 
-                math[] mvA = fd.getmathChildren();
+                math m_v = fd.getmathChild();
+
+                apply[] appA = m_v.getapplyChildren();
+                for (int k = 0; k < appA.length; k++) {
+                    apply app = appA[k];
+
+                    cn[] cnA = app.getcnChildren();
+                    for (int l = 0; l < cnA.length; l++) {
+                        cn cn_v = cnA[l];
+                        System.out.println("cntype " + cn_v.gettype());
+                        System.out.println("cnval " + cn_v.getvalue());
+                    }
+
+                    ci[] ciA = app.getciChildren();
+                    for (int l = 0; l < ciA.length; l++) {
+                        ci ci_v = ciA[l];
+                        System.out.println("cival " + ci_v.getvalue());
+                    }
+                }
+
+                lambda[] lmA = m_v.getlambdaChildren();
+                for (int k = 0; k < lmA.length; k++) {
+                    lambda lm_v = lmA[k];
+
+                    apply[] appA2 = lm_v.getapplyChildren();
+                    for (int l = 0; l < appA2.length; l++) {
+                        apply app = appA2[l];
+
+                        cn[] cnA = app.getcnChildren();
+                        for (int x = 0; x < cnA.length; x++) {
+                            cn cn_v = cnA[x];
+                            System.out.println("cntype " + cn_v.gettype());
+                            System.out.println("cnval " + cn_v.getvalue());
+                        }
+
+                        ci[] ciA = app.getciChildren();
+                        for (int x = 0; x < ciA.length; x++) {
+                            ci ci_v = ciA[x];
+                            System.out.println("cival " + ci_v.getvalue());
+                        }
+                    }
+                }
+            }
+        }
+
+        // listOfEvents
+        listOfEvents lEv = m.getlistOfEventsChild();
+        if (lEv != null) {
+            event[] evA = lEv.geteventChildren();
+            for (int i = 0; i < evA.length; i++) {
+                event e_v = evA[i];
+
+                delay d_v = e_v.getdelayChild();
+
+                math[] mvA = d_v.getmathChildren();
                 for (int j = 0; j < mvA.length; j++) {
                     math m_v = mvA[j];
 
@@ -195,66 +346,6 @@ public class ReadStringTest extends TestCase {
                             for (int x = 0; x < ciA.length; x++) {
                                 ci ci_v = ciA[x];
                                 System.out.println("cival " + ci_v.getvalue());
-                            }
-                        }
-                    }
-                }
-            }
-        }
-
-        // listOfEvents
-        listOfEvents lEv = m.getlistOfEventsChild();
-        if (lEv != null) {
-            event[] evA = lEv.geteventChildren();
-            for (int i = 0; i < evA.length; i++) {
-                event e_v = evA[i];
-
-                delay[] deA = e_v.getdelayChildren();
-                for (int ii = 0; ii < deA.length; ii++) {
-                    delay d_v = deA[ii];
-
-                    math[] mvA = d_v.getmathChildren();
-                    for (int j = 0; j < mvA.length; j++) {
-                        math m_v = mvA[j];
-
-                        apply[] appA = m_v.getapplyChildren();
-                        for (int k = 0; k < appA.length; k++) {
-                            apply app = appA[k];
-
-                            cn[] cnA = app.getcnChildren();
-                            for (int l = 0; l < cnA.length; l++) {
-                                cn cn_v = cnA[l];
-                                System.out.println("cntype " + cn_v.gettype());
-                                System.out.println("cnval " + cn_v.getvalue());
-                            }
-
-                            ci[] ciA = app.getciChildren();
-                            for (int l = 0; l < ciA.length; l++) {
-                                ci ci_v = ciA[l];
-                                System.out.println("cival " + ci_v.getvalue());
-                            }
-                        }
-
-                        lambda[] lmA = m_v.getlambdaChildren();
-                        for (int k = 0; k < lmA.length; k++) {
-                            lambda lm_v = lmA[k];
-
-                            apply[] appA2 = lm_v.getapplyChildren();
-                            for (int l = 0; l < appA2.length; l++) {
-                                apply app = appA2[l];
-
-                                cn[] cnA = app.getcnChildren();
-                                for (int x = 0; x < cnA.length; x++) {
-                                    cn cn_v = cnA[x];
-                                    System.out.println("cntype " + cn_v.gettype());
-                                    System.out.println("cnval " + cn_v.getvalue());
-                                }
-
-                                ci[] ciA = app.getciChildren();
-                                for (int x = 0; x < ciA.length; x++) {
-                                    ci ci_v = ciA[x];
-                                    System.out.println("cival " + ci_v.getvalue());
-                                }
                             }
                         }
                     }
