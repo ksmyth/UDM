bea4898a2ec3e7d4b0c5ff2c4cf4edcc747f2593
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:37:30 +0000

    date	2005.04.14.14.52.00;	author endre;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1768 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- include/UdmDom.h
+++ include/UdmDom.h
@@ -141,7 +141,7 @@ namespace UdmDom
 			str_str_map::value_type item(key, xsd_str);
 			pair<str_str_map::const_iterator, bool> ins_res = static_xsd_container.insert(item);
       const std::string& tmp = ins_res.first->second;
-      if (ins_res.second &&  (xsd_str!=tmp)) // only if it differs from the stored one Kalmar
+      if (!ins_res.second && (xsd_str!=tmp)) // only if it differs from the stored one Kalmar
 				throw udm_exception(string("An XSD by that name is already stored with a different content: ") + key);
 
 
@@ -154,6 +154,18 @@ namespace UdmDom
 
 		};
 	};
+
+
+	//map namespace URI to UML namespace
+	struct xsd_ns_mapping_storage
+	{
+		typedef map<const string, const string> str_str_map;
+		static UDM_DLL str_str_map static_xsd_ns_mapping_container;
+	};
+
+	UDM_DLL void AddURIToUMLNamespaceMapping(const string & namespaceURI, const string & namespaceUML);
+	UDM_DLL void RemoveURIToUMLNamespaceMapping(const string & namespaceURI);
+	UDM_DLL void ClearURIToUMLNamespaceMappings();
 
 
 	string GetTime();
