34c9e06267028125fd3aa7bee82a9c91db8ecae6
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:49:29 +0000

    date	2005.06.07.16.04.00;	author endre;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2044 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ src/Udm/Udm.h
@@ -0,0 +1,68 @@
+/*
+Copyright (c) Vanderbilt University, 2000-2001
+ALL RIGHTS RESERVED
+Vanderbilt University disclaims all warranties with regard to this
+software, including all implied warranties of merchantability
+and fitness.  In no event shall Vanderbilt University be liable for
+any special, indirect or consequential damages or any damages
+whatsoever resulting from loss of use, data or profits, whether
+in an action of contract, negligence or other tortious action,
+arising out of or in connection with the use or performance of
+this software.
+*/
+
+#ifndef MOBIES_UDM_H
+#define MOBIES_UDM_H
+
+#include <UdmDom.h>
+#include <UmlExt.h>
+
+
+enum CPPSourceUnit {
+	CPP_SOURCE_UNIT_DIAGRAM,	// one .h and one .cpp file per diagram
+	CPP_SOURCE_UNIT_NAMESPACE,	// one .h and one .cpp file per namespace
+	CPP_SOURCE_UNIT_CLASS,		// one .h and one .cpp file per namespace, one .h file per class
+};
+
+//cross-link-approved
+void GenerateHExport(const ::Uml::Uml::Diagram &diagram,  ostream &output, string fname, const string& macro);
+void GenerateH(const ::Uml::Uml::Diagram &diagram,  ostream &output, string fname, bool visitor_sup = false, const ::Uml::Uml::Diagram& cross_dgr = NULL, const string& macro = "", const int source_unit = CPP_SOURCE_UNIT_DIAGRAM);
+void GenerateHH(const ::Uml::Uml::Diagram &dgr, const std::string &fname, const bool visitor_sup, const ::Uml::Uml::Diagram &cross_dgr, const std::string &macro, int source_unit);
+void GenerateNewCPP(const ::Uml::Uml::Diagram &diagram, 
+                    string fname, 
+                    const map<string, string> &ns_map, 
+                    const ::Uml::Uml::Diagram& cross_dgr = NULL, 
+                    const string& macro = "",
+                    bool integrate_xsd = false,
+					int source_unit = CPP_SOURCE_UNIT_DIAGRAM);
+
+void GenerateCPP(	const ::Uml::Uml::Diagram &diagram, 
+					ostream &output, string fname, 
+					const ::Uml::Uml::Diagram& cross_dgr = NULL, 
+					const string& macro = "",
+					bool integrate_xsd = false);
+//void GenerateCORBACPP(const ::Uml::Uml::Diagram &diagram, ostream &output, string fname, const ::Uml::Uml::Diagram& cross_dgr = NULL, const string& macro = "");
+
+// not cross-link approved
+void GenerateCSInit(const ::Uml::Uml::Diagram &diagram, ostream &output, string fname);
+void GenerateNewCSInit(const ::Uml::Uml::Diagram &diagram, ostream &output, string fname);
+void GenerateCSApi(const ::Uml::Uml::Diagram &diagram, ostream &output, string fname);
+void GenerateCS(const ::Uml::Uml::Diagram &diagram,  ostream &output, string fname);
+void GenerateNewCS(const ::Uml::Uml::Diagram &diagram,  ostream &output, string fname);
+void CheckDiagram(const ::Uml::Uml::Diagram & diagram);
+
+// utils
+std::string GetTime();
+std::string UmlClassCPPName(const ::Uml::Uml::Class &cl);
+std::string NameToFilename(const std::string src);
+//generate DTD (only if asked) and XSD
+void GenerateDSD(const ::Uml::Uml::Namespace &ns,
+		 const string &fname,
+		 bool generate_dtd,
+		 bool uxsdi,
+		 bool xsd_el_ta,
+		 const map<string, string> &ns_map,
+		 const set<string> &ns_ignore_set,
+		 bool qualifiedAtrrsNS);
+
+#endif //MOBIES_UDM_H
