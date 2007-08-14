403af7596f428a93d3e755036fe7445e396c31a8
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 21:23:14 +0000

    date	2006.11.05.05.04.00;	author endre;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2618 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- tests/test_ns/main.cpp
+++ tests/test_ns/main.cpp
@@ -1,18 +1,24 @@
 // test_ns.cpp : Defines the entry point for the console application.
 //
 
-
 #include "test_ns.h"
 
 void writeTest(const std::string& fname)
 {
 
 	Udm::SmartDataNetwork  out(test_ns::diagram);
-  out.CreateNew(fname.c_str(), "test_ns_AB.xsd",	test_ns::AB::AB::meta, Udm::CHANGES_PERSIST_ALWAYS);
+	out.CreateNew(fname.c_str(), "test_ns_AB.xsd",	test_ns::AB::AB::meta, Udm::CHANGES_PERSIST_ALWAYS);
 
-  test_ns::AB::AB ab = test_ns::AB::AB::Cast(out.GetRootObject());
-  test_ns::B::B1 b1 = test_ns::B::B1::Create(ab);
-  test_ns::AB::AB1 ab1 = test_ns::AB::AB1::Create(ab);
+	test_ns::AB::AB ab = test_ns::AB::AB::Cast(out.GetRootObject());
+
+	test_ns::A::A1 a1 = test_ns::A::A1::Create(ab);
+	test_ns::A::AA::A1 a_aa1 = test_ns::A::AA::A1::Create(ab);
+	test_ns::B::B1 b1 = test_ns::B::B1::Create(ab);
+	test_ns::AB::AB1 ab1 = test_ns::AB::AB1::Create(ab);
+	test_ns::A::AA::A2 a2 = test_ns::A::AA::A2::Create(ab);
+	test_ns::AB::ABAB::AB2 ab2 = test_ns::AB::ABAB::AB2::Create(ab, test_ns::AB::AB::meta_ab2);
+
+	ab2.dst() += a2;
 
 	out.CloseWithUpdate();
 }
@@ -22,13 +28,11 @@ void readTest(const std::string& fname)
 {
 
 	Udm::SmartDataNetwork  out(test_ns::diagram);
-  out.OpenExisting(fname.c_str(), "test_ns_AB.xsd");
+	out.OpenExisting(fname.c_str(), "test_ns_AB.xsd");
 
-  
-  test_ns::AB::AB ab = test_ns::AB::AB::Cast(out.GetRootObject());
+	test_ns::AB::AB ab = test_ns::AB::AB::Cast(out.GetRootObject());
 
-	
-  out.CloseNoUpdate();
+	out.CloseNoUpdate();
 }
 //=========================================
 
