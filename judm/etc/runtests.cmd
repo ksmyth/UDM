4f94109b60b91f88f9a601231ce4ea4927fdd1c5
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:40:03 +0000

    date	2005.07.06.06.09.00;	author nsomogyi;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1879 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/etc/runtests.cmd
+++ judm/etc/runtests.cmd
@@ -1,2 +1,7 @@
 @rem Used in domain-specific packages to run unit tests
-@java -Djava.library.path=..\judm -jar @DSPACKAGE@-test.jar
+
+@set JUNIT_HOME=%UDM_3RDPARTY_PATH%\junit
+
+@set CLASSPATH=.;%JUNIT_HOME%;%JUNIT_HOME%\junit.jar;%UDM_PATH%\bin\judm.jar;%UDM_PATH%\samples\Java\@DSPACKAGE@\@DSPACKAGE@.jar;%UDM_PATH%\samples\Java\@DSPACKAGE@\@DSPACKAGE@-test.jar
+
+@java junit.textui.TestRunner edu.vanderbilt.isis.@DSPACKAGE_LOWER@.test.AllTests
