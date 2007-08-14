bdd287dffb1434186f5b88e4137ecf5dc61aeeb7
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:47:15 +0000

    date	2005.07.06.13.45.00;	author endre;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1996 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- samples/cross_links/cross_links.cpp
+++ samples/cross_links/cross_links.cpp
@@ -25,13 +25,13 @@ int main_static(int argc, char * argv[])
 		Udm::SmartDataNetwork sm2(CD2::diagram);
 		Udm::SmartDataNetwork sm3(CD3::diagram);
 
-		sm1.CreateNew("sm1.xml", "CD1", CD1::M1::meta);
-		sm2.CreateNew("sm2.xml", "CD2", CD2::M2::meta);
-		sm3.CreateNew("sm3.xml", "CD3", CD3::MM3::meta);
+		sm1.CreateNew("sm1.xml", "CD1", CD1::CD1::M1::meta);
+		sm2.CreateNew("sm2.xml", "CD2", CD2::CD2::M2::meta);
+		sm3.CreateNew("sm3.xml", "CD3", CD3::CD3::MM3::meta);
 
-		CD1::M1 m1 = CD1::M1::Cast(sm1.GetRootObject());
-		CD2::M2 m2 = CD2::M2::Cast(sm2.GetRootObject());
-		CD3::MM3 m3 = CD3::MM3::Cast(sm3.GetRootObject());
+		CD1::CD1::M1 m1 = CD1::CD1::M1::Cast(sm1.GetRootObject());
+		CD2::CD2::M2 m2 = CD2::CD2::M2::Cast(sm2.GetRootObject());
+		CD3::CD3::MM3 m3 = CD3::CD3::MM3::Cast(sm3.GetRootObject());
 
 		/*
 			Create a static udm project which does not offer
@@ -58,7 +58,7 @@ int main_static(int argc, char * argv[])
 		*/
 
 		m2.dst() = m1;
-		set<CD1::M1> a; a.insert(m1);
+		set<CD1::CD1::M1> a; a.insert(m1);
 		m3.src() = a;
 
 
@@ -78,9 +78,9 @@ int main(int argc, char* argv[])
 			Udm::UdmProject pr;
 
 			//specifying instance data networks which will be present in the project
-			Udm::DataNetworkSpecifier cd1_dn("CD1_inst.xml", "CD1", CD1::M1::meta);
-			Udm::DataNetworkSpecifier cd2_dn("CD2_inst.xml", "CD2", CD2::M2::meta);
-			Udm::DataNetworkSpecifier cd3_dn("CD3_inst.xml", "CD3", CD3::MM3::meta);
+			Udm::DataNetworkSpecifier cd1_dn("CD1_inst.xml", "CD1", CD1::CD1::M1::meta);
+			Udm::DataNetworkSpecifier cd2_dn("CD2_inst.xml", "CD2", CD2::CD2::M2::meta);
+			Udm::DataNetworkSpecifier cd3_dn("CD3_inst.xml", "CD3", CD3::CD3::MM3::meta);
 
 			//putting them in a vector
 			vector<Udm::DataNetworkSpecifier> dnsvec;
@@ -97,9 +97,9 @@ int main(int argc, char* argv[])
 			Udm::DataNetwork& cd3 = pr.GetDataNetwork("CD3_inst.xml");
 
 			//obtaining handles to the root objects (this is normal - existing - Udm operation)
-			CD1::M1 m1 = CD1::M1::Cast(cd1.GetRootObject());
-			CD2::M2 m2 = CD2::M2::Cast(cd2.GetRootObject());
-			CD3::MM3 m3 = CD3::MM3::Cast(cd3.GetRootObject());
+			CD1::CD1::M1 m1 = CD1::CD1::M1::Cast(cd1.GetRootObject());
+			CD2::CD2::M2 m2 = CD2::CD2::M2::Cast(cd2.GetRootObject());
+			CD3::CD3::MM3 m3 = CD3::CD3::MM3::Cast(cd3.GetRootObject());
 
 			/*
 			//assoc. class based link - setting up by setting the ends 
@@ -109,11 +109,11 @@ int main(int argc, char* argv[])
 
 
 			//test
-			CD2::M2 m2_too = m1.src_end();
+			CD2::CD2::M2 m2_too = m1.src_end();
 			if (m2 != m2_too)
 				throw udm_exception("Test failed!");
 
-			CD2::M3 m3_too = m1.dst_end();
+			CD3::CD2::M3 m3_too = m1.dst_end();
 			if (m3 != m3_too)
 				throw udm_exception("Test failed!");
 
@@ -122,7 +122,7 @@ int main(int argc, char* argv[])
 			//assoc. class based link - setting up by setting the links from the ends
 
 			
-			set<CD1::M1> a;
+			set<CD1::CD1::M1> a;
 			m2.dst() = m1;
 			 a.insert(m1);
 			m3.src() = a;
@@ -130,11 +130,11 @@ int main(int argc, char* argv[])
 
 			//test
 
-			CD1::M1 m1_too = m2.dst();
+			CD1::CD1::M1 m1_too = m2.dst();
 			if (m1 != m1_too)
 				throw udm_exception("Test failed!");
 
-			set<CD1::M1> a_too = m3.src();
+			set<CD1::CD1::M1> a_too = m3.src();
 			if (a_too.size() != a.size()) 
 				throw udm_exception("Test failed!");
 			if (*(a_too.begin()) != m1)
@@ -148,21 +148,21 @@ int main(int argc, char* argv[])
 			m1.CreateLink(m3, ai);
 
 
-			set<CD1::M1, greater_equal<CD1::M1> > a_tooo = m3.src_sorted(greater_equal<CD1::M1>());
+			set<CD1::CD1::M1, greater_equal<CD1::CD1::M1> > a_tooo = m3.src_sorted(greater_equal<CD1::CD1::M1>());
 			
 			
 /*			
 			//simple links
 			//setting a cross-network  simple link
-			set<CD2::M2> s; s.insert(m2);
+			set<CD2::CD2::M2> s; s.insert(m2);
 			m1.dst() = s;
 
 			//getting a cross-network simple link
-			set<CD2::M2> ss = m1.dst();
+			set<CD2::CD2::M2> ss = m1.dst();
 */			
 
 
-			set<Uml::AssociationRole> asr = Uml::AncestorCrossAssociationRoles(::CD3::MM3::meta, *CL::diagram.dgr);
+			set<Uml::AssociationRole> asr = Uml::AncestorCrossAssociationRoles(CD3::CD3::MM3::meta, Uml::GetTheOnlyNamespace(*CL::diagram.dgr));
 			cout << asr.size() << endl;
 
 
