89db92dba1e1ea652058b012feaf0903bf8e9e57
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:37:35 +0000

    date	2005.07.06.14.36.00;	author nsomogyi;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@1795 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- etc/JAVA/eclipseAPI.bat
+++ etc/JAVA/eclipseAPI.bat
@@ -1,18 +1,15 @@
-cls
-
 @echo off
 
 rem if "%3"=="" goto usage
 
-chdir > d
-set /p curr_dir=<"d"
+cd %4
 
 echo Generate domain-specific %1 jUdm source ...
 %UDM_PATH%\bin\udm.exe %2 -java %3
 echo Done
 
 echo Generate the domain-specific %1 eclipse project ...
-%UDM_3RDPARTY_PATH%\ant-1.6.2\bin\ant -f %UDM_PATH%\etc\java\etc\build.xml -Dbasedir=%curr_dir% -Ddspackage=%1 -quiet eclipse
+%UDM_3RDPARTY_PATH%\ant-1.6.2\bin\ant -f %UDM_PATH%\etc\java\etc\build.xml -Dbasedir="%4" -Ddspackage=%1 -Dout.dir="%5" -quiet eclipse
 echo Done
 
 goto end
@@ -20,7 +17,7 @@ goto end
 :usage
 echo .
 echo eclipseApi: Wrong parameter!
-echo Usage:   %%UDM_PATH%%\etc\java\eclipseAPI [eclipse_project_name] [UML_meta_model] [UDM_parameters]
-echo Example: %%UDM_PATH%%\etc\java\eclipseAPI SBML2Ex SBML2Ex_udm.xml "-i jigcell -q jigcell -e"
+echo Usage:   %%UDM_PATH%%\etc\java\eclipseAPI [eclipse_project_name] [UML_meta_model] [UDM_parameters] [working_dir]
+echo Example: %%UDM_PATH%%\etc\java\eclipseAPI SBML2Ex SBML2Ex_udm.xml "-i jigcell -q jigcell -e" . 
 goto end
 :end
--- etc/JAVA/libAPI.bat
+++ etc/JAVA/libAPI.bat
@@ -1,18 +1,13 @@
-cls
-
 @echo off
 
-rem if "%3"=="" goto usage
+cd %4
 
-chdir > d
-set /p curr_dir=<"d"
-
-echo Generate domain-specific %1 jUdm source ...
-%UDM_PATH%\bin\udm.exe %2 -java %3
+echo Generate domain-specific %1 source ...
+%UDM_PATH%\bin\udm.exe %2 -j %3
 echo Done
 
-echo Generate the domain-specific %1 jUdm jar ...
-%UDM_3RDPARTY_PATH%\ant-1.6.2\bin\ant -f %UDM_PATH%\etc\java\etc\build.xml -Dbasedir=%curr_dir% -Ddspackage=%1 -quiet lib
+echo Generate the domain-specific %1 jar ...
+%UDM_3RDPARTY_PATH%\ant-1.6.2\bin\ant -f %UDM_PATH%\etc\java\etc\build.xml -Dbasedir="%4" -Ddspackage=%1 -Dout.dir="%5" -quiet lib
 echo Done
 
 goto end
@@ -20,7 +15,7 @@ goto end
 :usage
 echo .
 echo judmApi: Wrong parameter!
-echo Usage:   %%UDM_PATH%%\etc\java\libAPI [library_name] [UML_meta_model] [UDM_parameters]
-echo Example: %%UDM_PATH%%\etc\java\libAPI SBML2Ex SBML2Ex_udm.xml "-i jigcell -q jigcell -e"
+echo Usage:   %%UDM_PATH%%\etc\java\libAPI [library_name] [UML_meta_model] [UDM_parameters] [working_dir]
+echo Example: %%UDM_PATH%%\etc\java\libAPI SBML2Ex SBML2Ex_udm.xml "-i jigcell -q jigcell -e" .
 goto end
 :end
