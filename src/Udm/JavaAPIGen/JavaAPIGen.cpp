a3770af2f48b1c2b1e0d6e76d5515194406eb792
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2007-08-14 20:52:24 +0000

    date	2005.05.11.11.27.00;	author kalmar;	state Exp;
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2055 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ src/Udm/JavaAPIGen/JavaAPIGen.cpp
@@ -0,0 +1,22 @@
+#include ".\javaapigen.h"
+
+void GenerateJava(const ::Uml::Uml::Diagram &diagram
+, const map<string, string> & ns_map
+, const string& inputfile);
+
+
+JavaAPIGen::JavaAPIGen(void)
+{
+}
+
+JavaAPIGen::~JavaAPIGen(void)
+{
+}
+//================================================
+void JavaAPIGen::GenerateJava(const ::Uml::Uml::Diagram &diagram, 
+                  const map<std::string, std::string>& ns_map, 
+                  const std::string& inputfile)
+{
+  GenerateJava(diagram,ns_map, inputfile);
+
+}
---
+++ src/Udm/JavaAPIGen/JavaAPIGen.h
@@ -0,0 +1,14 @@
+#ifndef JavaAPIGen_H
+#define JavaAPIGen_H
+#include <Uml.h>
+
+class JavaAPIGen
+{
+public:
+  JavaAPIGen(void);
+  ~JavaAPIGen(void);
+  void GenerateJava(const ::Uml::Uml::Diagram &diagram, 
+                    const map<std::string, std::string>& ns_map, 
+                    const std::string& inputfile);
+};
+#endif //JavaAPIGen_H
