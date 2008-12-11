07c01c2b7c0bb7e7f11260efbe1e607bf5ed8610
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2008-12-11 21:33:08 +0000

    Removed file/folder
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2780 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/build/build_win/GeneTF/runtests.cmd
+++
@@ -1,7 +0,0 @@
-@rem Used in domain-specific packages to run unit tests
-
-@set JUNIT_HOME=%UDM_3RDPARTY_PATH%\junit
-
-@set CLASSPATH=.;%JUNIT_HOME%;%JUNIT_HOME%\junit.jar;%UDM_PATH%\bin\judm.jar;%UDM_PATH%\samples\Java\GeneTF\GeneTF.jar;%UDM_PATH%\samples\Java\GeneTF\GeneTF-test.jar
-
-@java junit.textui.TestRunner edu.vanderbilt.isis.genetf.test.AllTests
