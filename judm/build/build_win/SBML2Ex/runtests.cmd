8f7d8adc4fedf90330767a9a552d007910b2e423
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2008-12-11 21:33:26 +0000

    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2782 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/build/build_win/SBML2Ex/runtests.cmd
+++
@@ -1,7 +0,0 @@
-@rem Used in domain-specific packages to run unit tests
-
-@set JUNIT_HOME=%UDM_3RDPARTY_PATH%\junit
-
-@set CLASSPATH=.;%JUNIT_HOME%;%JUNIT_HOME%\junit.jar;%UDM_PATH%\bin\judm.jar;%UDM_PATH%\samples\Java\SBML2Ex\SBML2Ex.jar;%UDM_PATH%\samples\Java\SBML2Ex\SBML2Ex-test.jar
-
-@java junit.textui.TestRunner edu.vanderbilt.isis.sbml2ex.test.AllTests
