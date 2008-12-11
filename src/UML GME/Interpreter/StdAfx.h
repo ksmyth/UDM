010a305f01f7ed823ffff535845cde9190ab9301
endre <endre@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2008-12-11 07:00:16 +0000

    Solutions and projects for Visual Studio 2008.
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2771 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- src/UML GME/Interpreter/StdAfx.h
+++ src/UML GME/Interpreter/StdAfx.h
@@ -11,7 +11,11 @@
 
 #define STRICT
 #ifndef _WIN32_WINNT
+#if _MSC_VER >= 1500
+#define _WIN32_WINNT 0x0500		// Windows 2000 is the minimal version supported by VS 2008
+#else
 #define _WIN32_WINNT 0x0400
+#endif
 #endif
 #define _ATL_APARTMENT_THREADED
 
--- src/UdmGme/CommonHeaders.h
+++ src/UdmGme/CommonHeaders.h
@@ -1,6 +1,10 @@
 #define STRICT
 #ifndef _WIN32_WINNT
+#if _MSC_VER >= 1500
+#define _WIN32_WINNT 0x0500		// Windows 2000 is the minimal version supported by VS 2008
+#else
 #define _WIN32_WINNT 0x0400
+#endif
 #endif
 #include <windows.h>
 #include <cstdio>
