75ff8383d1f9d721164df53974c5e81ae783ffdc
fengshi <fengshi@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2008-12-11 21:33:32 +0000

    Removed file/folder
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2783 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- judm/build/build_win/TimeSeries/runtests.cmd
+++
@@ -1,7 +0,0 @@
-@rem Used in domain-specific packages to run unit tests
-
-@set JUNIT_HOME=%UDM_3RDPARTY_PATH%\junit
-
-@set CLASSPATH=.;%JUNIT_HOME%;%JUNIT_HOME%\junit.jar;%UDM_PATH%\bin\judm.jar;%UDM_PATH%\samples\Java\TimeSeries\TimeSeries.jar;%UDM_PATH%\samples\Java\TimeSeries\TimeSeries-test.jar
-
-@java junit.textui.TestRunner edu.vanderbilt.isis.timeseries.test.AllTests
