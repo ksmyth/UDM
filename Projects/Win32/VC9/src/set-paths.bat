1f904c0ba2626d41e2b0448a42d4503d349a8b72
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2009-08-20 14:11:51 +0000

    Use boost-1.34.1 instead of boost
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@2909 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- Projects/Win32/VC9/src/set-paths.bat
+++ Projects/Win32/VC9/src/set-paths.bat
@@ -8,6 +8,6 @@ if not exist "%JDK_PATH%" echo Set JDK_PATH & goto :eof
 if not exist "%GME_ROOT%" echo Set GME_ROOT & goto :eof
 
 set PATH=%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\bin;%UDM_3RDPARTY_PATH%\xalan-c_1_11_0\bin;%UDM_3RDPARTY_PATH%\cppunit-1.12.1\bin;%UDM_3RDPARTY_PATH%\info-zip;%UDM_3RDPARTY_PATH%\sed;%UDM_3RDPARTY_PATH%\swigwin-1.3.36;%PATH%
-set INCLUDE=%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\include;%UDM_3RDPARTY_PATH%\Xalan-C_1_11_0\include;%UDM_3RDPARTY_PATH%\antlr-2.7.7\lib\cpp;%UDM_3RDPARTY_PATH%\zlib;%UDM_3RDPARTY_PATH%\zlib\contrib\minizip;%UDM_3RDPARTY_PATH%\cppunit-1.12.1\include;%UDM_3RDPARTY_PATH%\boost;%JDK_PATH%\include\win32;%JDK_PATH%\include;%GME_ROOT%\Include;%GME_ROOT%\Gme\Include;%GME_ROOT%\Interfaces;%GME_ROOT%\Gme\Interfaces
+set INCLUDE=%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\include;%UDM_3RDPARTY_PATH%\Xalan-C_1_11_0\include;%UDM_3RDPARTY_PATH%\antlr-2.7.7\lib\cpp;%UDM_3RDPARTY_PATH%\zlib;%UDM_3RDPARTY_PATH%\zlib\contrib\minizip;%UDM_3RDPARTY_PATH%\cppunit-1.12.1\include;%UDM_3RDPARTY_PATH%\boost-1.34.1;%JDK_PATH%\include\win32;%JDK_PATH%\include;%GME_ROOT%\Include;%GME_ROOT%\Gme\Include;%GME_ROOT%\Interfaces;%GME_ROOT%\Gme\Interfaces
 set LIB=%UDM_3RDPARTY_PATH%\Xalan-C_1_11_0\lib;%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\lib;%UDM_3RDPARTY_PATH%\zlib;%UDM_3RDPARTY_PATH%\antlr-2.7.7\lib;%UDM_3RDPARTY_PATH%\cppunit-1.12.1\lib;%UDM_3RDPARTY_PATH%\boost-1.34.1\lib;%JDK_PATH%\lib
 set CLASSPATH=%UDM_3RDPARTY_PATH%\antlr-2.7.7\antlr.jar;%CLASSPATH%
