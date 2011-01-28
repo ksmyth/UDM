59b735b8942128b2f8fcfb4ced74f4d235ed9916
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-01-28 22:53:58 +0000

    Use .vsprops for INCLUDE, swig.exe, LIB, antlr CLASSPATH
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3625 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- scripts/vc9-env.bat
+++ scripts/vc9-env.bat
@@ -23,35 +23,12 @@ set PATH=%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\bin;^
 %UDM_3RDPARTY_PATH%\cppunit-1.12.1\bin;^
 %UDM_3RDPARTY_PATH%\info-zip;^
 %UDM_3RDPARTY_PATH%\sed;^
-%UDM_3RDPARTY_PATH%\swigwin-2.0.0;^
 %UDM_3RDPARTY_PATH%\ant-1.6.2\bin;^
 %PATH%
 
-set INCLUDE=%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\include;^
-%UDM_3RDPARTY_PATH%\Xalan-C_1_11_0\include;^
-%UDM_3RDPARTY_PATH%\antlr-2.7.7\lib\cpp;^
-%UDM_3RDPARTY_PATH%\zlib;^
-%UDM_3RDPARTY_PATH%\zlib\contrib\minizip;^
-%UDM_3RDPARTY_PATH%\cppunit-1.12.1\include;^
-%UDM_3RDPARTY_PATH%\boost_1_36_0;^
-%JDK_PATH%\include\win32;^
-%JDK_PATH%\include;^
-%GME_ROOT%\Interfaces;^
-%GME_ROOT%\Gme\Interfaces;^
-%INCLUDE%
-
-set LIB=%UDM_3RDPARTY_PATH%\Xalan-C_1_11_0\lib;^
-%UDM_3RDPARTY_PATH%\xerces-c_2_8_0\lib;^
-%UDM_3RDPARTY_PATH%\zlib;^
-%UDM_3RDPARTY_PATH%\antlr-2.7.7\lib;^
-%UDM_3RDPARTY_PATH%\cppunit-1.12.1\lib;^
-%JDK_PATH%\lib;^
-%LIB%
-
 set JAVA_HOME=%JDK_PATH%
 
-set _CLASSPATH=%UDM_3RDPARTY_PATH%\junit\junit.jar;^
-%UDM_3RDPARTY_PATH%\antlr-2.7.7\antlr.jar
+set _CLASSPATH=$(UDM_3RDPARTY_PATH)\junit\junit.jar;$(UDM_3RDPARTY_PATH)\antlr-2.7.7\antlr.jar
 if defined CLASSPATH (set CLASSPATH=%_CLASSPATH%;%CLASSPATH%) else (set CLASSPATH=%_CLASSPATH%)
 
 set ANT_HOME=%UDM_3RDPARTY_PATH%\ant-1.6.2
