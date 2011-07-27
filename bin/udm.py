cebd8ab8efcfa45f4fc78cf34e9df1a86401f767
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-07-27 18:13:55 +0000

    Automatically load correct udm.pyd according to Python version
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3799 c0adbae9-c137-0410-b742-ec4b2d2ceded

---
+++ bin/udm.py
@@ -0,0 +1,7 @@
+import sys
+if sys.version_info[0:2] == (2,6):
+    from Python26.udm import *
+elif sys.version_info[0:2] == (2,7):
+    from Python27.udm import *
+else:
+    raise ImportError("Unsupported Python version " + str(sys.version_info))
