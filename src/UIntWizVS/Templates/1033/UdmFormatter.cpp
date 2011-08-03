5bfc43d4957d9e92ba8404dbc724e854766d4af6
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-08-03 22:32:09 +0000

    Fix compilation
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3819 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- src/UIntWizVS/Templates/1033/UdmFormatter.cpp
+++ src/UIntWizVS/Templates/1033/UdmFormatter.cpp
@@ -10,13 +10,15 @@ namespace GMEConsole
 {
 	std::string Formatter::MakeObjectHyperlink(const std::string & text, const Udm::Object& object)
 	{
+		ostringstream ostr;
 		ostr << "<a href=\"mga:" << UdmGme::UdmId2GmeId(object.uniqueId()) << "\">" << text << "</a>";
 		return ostr.str();
 	}
 
 	std::string Formatter::MakeColored(const std::string & text, COLORREF color)
 	{
-		ostringstream ostr; ostr << "<font color=\"#" << hex << setfill('0') << setw(2) << (int)GetRValue(color) 
+		ostringstream ostr;
+		ostr << "<font color=\"#" << hex << setfill('0') << setw(2) << (int)GetRValue(color) 
 			<<setw(2) << (int)GetGValue(color) <<setw(2) << (int)GetBValue(color) << "\">" << text <<"</font>";
 		
 		return ostr.str();
