12a9d008ccb762f74000d0856cee4dfb4f95c1a3
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-08-26 14:41:47 +0000

    Add test
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3853 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- tests/test_staticleaksdll/DllMain.cpp
+++ tests/test_staticleaksdll/DllMain.cpp
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <crtdbg.h>
 
-#include "../test_refports/RefPort.h"
+//#include "../test_refports/RefPort.h"
+#include "LampDiagram.h"
 
 #include "UdmBase.h"
 #include "UdmStatic.h"
@@ -12,6 +13,7 @@ __declspec(dllexport) void init(void* data) {
 	_CrtMemState* state = (_CrtMemState*) data;
 
 	Uml::Initialize();
-	RefPort::Initialize();
+	//RefPort::Initialize();
+	LampDiagram::Initialize();
 	UdmUtil::stacktrace();
 }
