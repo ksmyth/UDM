bb8db7da3104adb52208aef641ebfbbf1fb4fe77
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-09-21 15:47:29 +0000

    VC7.1 and 8 arent supported anymore
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3906 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- scripts/run-ant-on-judm.bat
+++ scripts/run-ant-on-judm.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if /i not "%1"=="VC7.1" if /i not "%1"=="VC8" if /i not "%1"=="VC9" goto :usage
+if /i not "%1"=="VC9" goto :usage
 if /i not "%2"=="Debug" if /i not "%2"=="Release" goto :usage
 
 rem Run ant
@@ -10,5 +10,5 @@ exit /b
 
 :usage
 echo Runs ant on judm, with the specified UDM build
-echo Usage: %0 ^<VC7.1^|VC8^|VC9^> ^<Debug^|Release^> ^<parameters to ant^>
+echo Usage: %0 ^<VC9^> ^<Debug^|Release^> ^<parameters to ant^>
 exit /b
