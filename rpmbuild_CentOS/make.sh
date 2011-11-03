abe506532138c7a7c140f8f66b17298f1e94a7a5
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-11-03 21:47:52 +0000

    Start from a clean install dir
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3929 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- rpmbuild_CentOS/make.sh
+++ rpmbuild_CentOS/make.sh
@@ -5,9 +5,10 @@
 set -e
 set -u
 
+if [ -d udm-$VERSION ]; then true rm -rf udm-$VERSION; fi
 pushd ..
 VERSION=3.13
-./configure LDFLAGS="-L$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/minizip/" CPPFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/"  CXXFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/" --with-antlr=antlr --with-pccts-dir=/usr/include/antlr/ --without-xalan --without-antlr --prefix=/usr
+./configure LDFLAGS="-L$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/minizip/" CPPFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/"  CXXFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/" --with-pccts-dir=/usr/include/antlr/ --without-xalan --without-antlr --prefix=/usr
 make DESTDIR=`pwd`/rpmbuild/udm-$VERSION install
 popd
 
