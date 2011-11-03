2672e7358137fb885cd02f1e0e540b3e62ac0293
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2011-11-03 21:59:51 +0000

    Minor fixes
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@3930 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- rpmbuild_CentOS/make.sh
+++ rpmbuild_CentOS/make.sh
@@ -1,15 +1,21 @@
 #!/bin/bash
 
+VERSION=3.13
+
 # http://stackoverflow.com/questions/880227/what-is-the-minimum-i-have-to-do-to-create-an-rpm-file
+
+#cat ~/.rpmmacros
+# %_topdir expanded_pwd
+# %_tmppath  %{_topdir}/tmp
 
 set -e
 set -u
 
 if [ -d udm-$VERSION ]; then true rm -rf udm-$VERSION; fi
+mkdir udm-$VERSION
 pushd ..
-VERSION=3.13
-./configure LDFLAGS="-L$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/minizip/" CPPFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/"  CXXFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/" --with-pccts-dir=/usr/include/antlr/ --without-xalan --without-antlr --prefix=/usr
-make DESTDIR=`pwd`/rpmbuild/udm-$VERSION install
+./configure LDFLAGS="-L$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/minizip/" CPPFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/"  CXXFLAGS="-I$(pwd)/debian/zlib-1.2.3.3.dfsg/contrib/" --without-xalan --without-antlr --prefix=/usr
+make DESTDIR=`pwd`/rpmbuild_CentOS/udm-$VERSION install
 popd
 
 tar -zcvf udm-$VERSION.tar.gz udm-$VERSION
