873ba9fdd814dc66b67434f0088ac8b8131c6f56
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2012-08-03 22:33:39 +0000

    More CrtReportHook so no MessageBoxes pop up
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@4076 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- tests/test_simpleudmops/LampTest.cpp
+++ tests/test_simpleudmops/LampTest.cpp
@@ -6,8 +6,46 @@
 #include <cppunit/ui/text/TestRunner.h>
 #include <cppunit/extensions/HelperMacros.h>
 
+#ifdef _WIN32
+#include <crtdbg.h>
+#include <signal.h>
+#include <windows.h>
+
+int __cdecl CrtReportHook(int reportType, char *message, int *returnValue)
+{
+	if (reportType == _CRT_ASSERT) {
+		DebugBreak();
+		return TRUE;
+	}
+	return FALSE;
+}
+
+void __cdecl AbrtHandler(int)
+{
+	DebugBreak();
+}
+
+struct _CrtHooksRAII {
+	_CrtHooksRAII() {
+		oldAbrtHandler = signal(SIGABRT, &AbrtHandler);
+		_CrtSetReportMode(_CRT_ASSERT, _CRTDBG_MODE_DEBUG);
+		_set_abort_behavior(0, _WRITE_ABORT_MSG);
+		oldHook = _CrtSetReportHook(&CrtReportHook);
+	}
+	~_CrtHooksRAII() {
+		_CrtSetReportHook(oldHook);
+		signal(SIGABRT, oldAbrtHandler);
+	}
+	_CRT_REPORT_HOOK oldHook;
+	void (__cdecl *oldAbrtHandler)(int);
+};
+#endif
+
 int main(int argc, char* argv[])
 {
+#ifdef _WIN32
+	_CrtHooksRAII hooks;
+#endif
 	CPPUNIT_NS::Test *suite = CPPUNIT_NS::TestFactoryRegistry::getRegistry().makeTest();
 
 	// Adds the test to the list of test to run
