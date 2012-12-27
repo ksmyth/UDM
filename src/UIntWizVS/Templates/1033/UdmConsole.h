bd67afae9edc83a17f68ca4ab09ed57c660b014e
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2012-12-27 18:37:52 +0000

    Unicode
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@4145 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- src/UIntWizVS/Templates/1033/UdmConsole.h
+++ src/UIntWizVS/Templates/1033/UdmConsole.h
@@ -12,6 +12,8 @@
 
 namespace GMEConsole
 {
+	CComBSTR BSTRFromUTF8(const std::string & utf8);
+
 	class Console
 	{
 	friend class RawComponent;
@@ -26,10 +28,10 @@ namespace GMEConsole
 		static void setupConsole(CComPtr<IMgaProject> project);
 		static void freeConsole()
 		{
-			if(gmeoleapp!=0)
+			if (gmeoleapp != 0)
 			{
 				gmeoleapp.Release();
-				gmeoleapp=0;
+				gmeoleapp = 0;
 			}
 		}
 		static void SetupConsole(CComPtr<IMgaProject> project) { setupConsole(project); }
@@ -41,15 +43,34 @@ namespace GMEConsole
 				case MSG_NORMAL:
 				case MSG_INFO:
 				case MSG_WARNING:
-					_tprintf("%s", message.c_str());
+					printf("%s", message.c_str());
 					break;
 				case MSG_ERROR:
-					_ftprintf(stderr, "%s", message.c_str());
+					fprintf(stderr, "%s", message.c_str());
 					break;
 				}
 			} else {
-				if(S_OK != gmeoleapp->ConsoleMessage( CComBSTR(message.length(),message.c_str()),type))
-					throw udm_exception("Could not write GME console.");
+				if(S_OK != gmeoleapp->ConsoleMessage(CComBSTR(message.length(), message.c_str()), type))
+					throw udm_exception("Could not write to GME console.");
+			}
+		}
+
+		static void writeLine(const std::wstring& message, msgtype_enum type)
+		{
+			if (gmeoleapp == 0) {
+				switch (type) {
+				case MSG_NORMAL:
+				case MSG_INFO:
+				case MSG_WARNING:
+					wprintf(L"%s", message.c_str());
+					break;
+				case MSG_ERROR:
+					fwprintf(stderr, L"%s", message.c_str());
+					break;
+				}
+			} else {
+				if(S_OK != gmeoleapp->ConsoleMessage(CComBSTR(message.length(), message.c_str()), type))
+					throw udm_exception("Could not write to GME console.");
 			}
 		}
 
@@ -61,15 +82,25 @@ namespace GMEConsole
 
 		static void setContents(const std::string& contents)
 		{
-			if(S_OK != gmeoleapp->put_ConsoleContents( CComBSTR(contents.length(),contents.c_str()) ))
-				throw udm_exception("Could not set the contents of GME console.");
-
+			if (gmeoleapp != 0)
+				if(S_OK != gmeoleapp->put_ConsoleContents( CComBSTR(contents.length(),contents.c_str()) ))
+					throw udm_exception("Could not set the contents of GME console.");
+		}
+		static void setContents(const std::wstring& contents)
+		{
+			if (gmeoleapp != 0)
+				if(S_OK != gmeoleapp->put_ConsoleContents( CComBSTR(contents.length(),contents.c_str()) ))
+					throw udm_exception("Could not set the contents of GME console.");
 		}
 
 		class Error
 		{
 		public:
 			static void writeLine(const std::string& message)
+			{
+				Console::writeLine(message,MSG_ERROR);
+			}
+			static void writeLine(const std::wstring& message)
 			{
 				Console::writeLine(message,MSG_ERROR);
 			}
@@ -81,6 +112,10 @@ namespace GMEConsole
 			{
 				Console::writeLine(message, MSG_NORMAL);
 			}
+			static void writeLine(const std::wstring& message)
+			{
+				Console::writeLine(message, MSG_NORMAL);
+			}
 		};
 		class Warning
 		{
@@ -89,11 +124,19 @@ namespace GMEConsole
 			{
 				Console::writeLine(message, MSG_WARNING);
 			}
+			static void writeLine(const std::wstring& message)
+			{
+				Console::writeLine(message, MSG_WARNING);
+			}
 		};
 		class Info
 		{
 		public:
 			static void writeLine(const std::string& message)
+			{
+				Console::writeLine(message,MSG_INFO);
+			}
+			static void writeLine(const std::wstring& message)
 			{
 				Console::writeLine(message,MSG_INFO);
 			}
