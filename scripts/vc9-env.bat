80ecd2e59050f502f7d5ab1426bc744a6ea74546
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2013-09-03 16:48:39 +0000

    Use JAVA_PATH everywhere, it is standard
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@4191 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- scripts/vc9-env.bat
+++ scripts/vc9-env.bat
@@ -1,10 +1,10 @@
 @echo off
 
-: Read UDM_3RDPARTY_PATH and JDK_PATH from script with local settings
+: Read UDM_3RDPARTY_PATH and JAVA_HOME from script with local settings
 if exist "%~dp0"vc9-env-local.bat call "%~dp0"vc9-env-local.bat
 
 if not defined UDM_3RDPARTY_PATH echo Set UDM_3RDPARTY_PATH & exit /b 1
-if not defined JDK_PATH echo Set JDK_PATH & exit /b 1
+if not defined JAVA_HOME echo Set JAVA_HOME & exit /b 1
 if not defined GME_ROOT echo Set GME_ROOT & exit /b 1
 
 if defined UDM_PATH goto :udm_path_set
@@ -23,8 +23,6 @@ set PATH=%UDM_PATH%\bin;^
 %UDM_3RDPARTY_PATH%\info-zip;^
 %UDM_3RDPARTY_PATH%\apache-ant-1.7.1\bin;^
 %PATH%
-
-set JAVA_HOME=%JDK_PATH%
 
 set _CLASSPATH=%UDM_3RDPARTY_PATH%\junit\junit.jar;%UDM_3RDPARTY_PATH%\antlr-2.7.7\antlr.jar
 if defined CLASSPATH (set CLASSPATH=%_CLASSPATH%;%CLASSPATH%) else (set CLASSPATH=%_CLASSPATH%)
