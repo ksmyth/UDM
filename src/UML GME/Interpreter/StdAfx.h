ad207af3f939eec11ed720bcfff6c92fb67ba5fa
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2014-03-04 21:35:21 +0000

    Fix warning
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@4258 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- src/UML GME/Interpreter/StdAfx.h
+++ src/UML GME/Interpreter/StdAfx.h
@@ -2,12 +2,9 @@
 //      or project specific include files that are used frequently,
 //      but are changed infrequently
 
-#if !defined(AFX_STDAFX_H__C4EFDDFC_C095_4509_B571_632F0986D162__INCLUDED_)
-#define AFX_STDAFX_H__C4EFDDFC_C095_4509_B571_632F0986D162__INCLUDED_
-
-#if _MSC_VER > 1000
 #pragma once
-#endif // _MSC_VER > 1000
+
+#define _CRT_NONSTDC_NO_DEPRECATE
 
 #define STRICT
 #ifndef _WIN32_WINNT
@@ -38,5 +35,3 @@ extern CComModule _Module;
 #include "UdmBase.h"
 #include "Uml.h"
 #include "UmlExt.h"
-
-#endif // !defined(AFX_STDAFX_H__C4EFDDFC_C095_4509_B571_632F0986D162__INCLUDED)
