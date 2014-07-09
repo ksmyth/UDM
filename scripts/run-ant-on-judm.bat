912740ebb48a18a9e0f24ede1e4de2fe65106e03
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2014-07-09 18:37:16 +0000

    Upgrade UdmSwig.dll in installer to VS2010
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@4310 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- scripts/run-ant-on-judm.bat
+++ scripts/run-ant-on-judm.bat
@@ -1,6 +1,6 @@
 @echo off
 
-if /i not "%1"=="VC9" goto :usage
+if /i not "%1"=="VC10" goto :usage
 if /i not "%2"=="Debug" if /i not "%2"=="Release" goto :usage
 
 rem Run ant
@@ -10,5 +10,5 @@ exit /b
 
 :usage
 echo Runs ant on judm, with the specified UDM build
-echo Usage: %0 ^<VC9^> ^<Debug^|Release^> ^<parameters to ant^>
+echo Usage: %0 ^<VC10^> ^<Debug^|Release^> ^<parameters to ant^>
 exit /b
---
+++ scripts/vc10-env.bat
@@ -0,0 +1,29 @@
+@echo off
+
+: Read UDM_3RDPARTY_PATH and JAVA_HOME from script with local settings
+if exist "%~dp0"vc10-env-local.bat call "%~dp0"vc10-env-local.bat
+
+if not defined UDM_3RDPARTY_PATH echo Set UDM_3RDPARTY_PATH & exit /b 1
+if not defined JAVA_HOME echo Set JAVA_HOME & exit /b 1
+if not defined GME_ROOT echo Set GME_ROOT & exit /b 1
+
+if defined UDM_PATH goto :udm_path_set
+
+rem Determine UDM_PATH
+pushd %CD%
+rem cd to the parent directory of this script
+cd /d %~dp0..
+set UDM_PATH=%CD%
+popd
+
+:udm_path_set
+
+set PATH=%UDM_PATH%\bin;^
+%UDM_3RDPARTY_PATH%\cppunit-1.12.1\bin;^
+%UDM_3RDPARTY_PATH%\info-zip;^
+%UDM_3RDPARTY_PATH%\apache-ant-1.7.1\bin;^
+%PATH%
+
+set _CLASSPATH=%UDM_3RDPARTY_PATH%\junit\junit.jar;%UDM_3RDPARTY_PATH%\antlr-2.7.7\antlr.jar
+if defined CLASSPATH (set CLASSPATH=%_CLASSPATH%;%CLASSPATH%) else (set CLASSPATH=%_CLASSPATH%)
+
