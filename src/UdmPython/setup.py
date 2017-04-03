60543458f5d564847274239e1bc6765a9f702246
ksmyth <ksmyth@c0adbae9-c137-0410-b742-ec4b2d2ceded>
2017-04-03 15:29:28 +0000

    Build udm.pyd ext in setup.py
    git-svn-id: https://svn.isis.vanderbilt.edu/MoBIES/UDM/trunk@4387 c0adbae9-c137-0410-b742-ec4b2d2ceded

--- src/UdmPython/setup.py
+++ src/UdmPython/setup.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 
+from __future__ import absolute_import
 import sys
 import os
 import os.path
 import platform
+import struct
 #from distutils.core import setup, Extension
 #from distutils.command.build_ext import build_ext
 from setuptools import setup, Extension
@@ -20,15 +22,14 @@ setup_args = dict(
     author_email='ksmyth@isis.vanderbilt.edu',
     url='http://repo.isis.vanderbilt.edu/',
     license='Vanderbilt UDM License',
-    packages=['udm'],
 )
 
 setup_args['ext_modules'] = [
-  Extension('udm.udm',
-    ['UdmPython.cpp'],
-    libraries=['boost_python', 'udm'], # TODO: udm?
-    library_dirs = [os.path.join(this_dir, '../../lib/.libs')],
-    include_dirs=['/usr/include/udm/', os.path.join(this_dir, '../../include')])
+    Extension('udm',
+              ['UdmPython.cpp'],
+              libraries=['boost_python', 'udm'],  # TODO: udm?
+              library_dirs=[os.path.join(this_dir, '../../lib/.libs')],
+              include_dirs=['/usr/include/udm/', os.path.join(this_dir, '../../include')])
 ]
 if platform.system() == 'Windows':
     class my_build_ext(build_ext):
@@ -40,12 +41,26 @@ if platform.system() == 'Windows':
             try:
                 os.makedirs(os.path.dirname(self.get_ext_fullpath(ext.name)))
             except WindowsError as e:
-                if e.winerror != 183: # already exists
+                if e.winerror != 183:  # already exists
                     raise
 
+            pythonbase = sys.exec_prefix
 
+            os.environ['LIB'] = os.path.join(pythonbase, 'libs')
+            # If building against Python source, may also need:
+            # \PCbuild\$(PlatformShortName)
+            # \PCbuild
+            os.environ['INCLUDE'] = os.path.join(pythonbase, 'include') + ';' + os.path.join(os.path.dirname(os.path.abspath(__file__)), 'windows_compat')
+            # \PC
+
+            import subprocess
+            subprocess.check_call([os.path.join(os.environ['VS100COMNTOOLS'], '..', '..', 'VC', 'vcvarsall.bat'), ('AMD64' if struct.calcsize("P") == 8 else 'x86')] +
+                ['&&'] +
+                [r'msbuild', r'..\..\Projects\Win32\VC10\src\UdmPython\UdmPython.vcxproj', '/t:Rebuild', '/fl',
+                '/p:Configuration=ReleaseEnv;Platform={0};UseEnv=true;PYTHON_VERSION={1[0]}{1[1]}'.format(('x64' if struct.calcsize("P") == 8 else 'win32'), sys.version_info[0:2])],
+                shell=True)
             shutil.copyfile(os.path.join(this_dir, r'..\..\bin\Python%d%d\udm.pyd' % sys.version_info[0:2]), self.get_ext_fullpath(ext.name))
 
-    setup_args['cmdclass'] = {'build_ext': my_build_ext }
+    setup_args['cmdclass'] = {'build_ext': my_build_ext}
 
 setup(**setup_args)
--- src/UdmPython/udm/__init__.py
+++
@@ -1,9 +0,0 @@
-'''
-Udm: Universal data model
-
-Udm is a metaprogrammable tool for providing uniform access to data structures that could be persistent.
-
-http://repo.isis.vanderbilt.edu/tools/get_tool?UDM
-'''
-
-from .udm import *
